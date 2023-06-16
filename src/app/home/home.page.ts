dde5a45a2ed224e997178ce8e5dcc6ba56243d99
FeMott4 <125033689+FeMott4@users.noreply.github.com>
2023-06-16 12:07:51 +0000

    exibindo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,13 +10,35 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule,CommonModule],
 })
 export class HomePage {
+  resultado=0;
+  zerar=0;
+  numero='number';
+  decimal=false;
+  decimals=0;
 
-  exibir(numero: number) {
-		alert(numero);
+  exibir(n: number) {
+		switch (this.numero) {
+      case 'number':
+        if (this.decimal) {
+          this.decimals++;
+          this.resultado = this.resultado + n * Math.pow(10, -this.decimals);
+        } else {
+          this.resultado = this.resultado * 10 + n;
+        }
+        break;
+      case 'operator':
+        this.resultado = n;
+        this.numero = 'number';
+        break;
+      case 'result':
+        this.zerar = 0;
+        this.resultado = n;
+        this.numero = 'number';
+    }
+  }
 	}
 
  
 
   
 
-}
