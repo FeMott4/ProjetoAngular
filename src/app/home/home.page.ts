4cfe2cabfd1ee281dcdde313d28fe8bb5fd77d38
FeMott4 <125033689+FeMott4@users.noreply.github.com>
2023-06-16 14:00:52 +0000

    final sem virgula

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
-@Component({
+@Component({ 
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
@@ -10,35 +10,66 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule,CommonModule],
 })
 export class HomePage {
-  resultado=0;
-  zerar=0;
+  // declarações
+  v2=0;
+  v1=0;
   numero='number';
+  operador='+'
   decimal=false;
   decimals=0;
 
+  // Função para salvar e exibir os dados inseridos
   exibir(n: number) {
 		switch (this.numero) {
       case 'number':
         if (this.decimal) {
           this.decimals++;
-          this.resultado = this.resultado + n * Math.pow(10, -this.decimals);
+          this.v2 = this.v2 + n * Math.pow(10, -this.decimals);
         } else {
-          this.resultado = this.resultado * 10 + n;
+          this.v2 = this.v2 * 10 + n;
         }
         break;
       case 'operator':
-        this.resultado = n;
+        this.v2 = n;
         this.numero = 'number';
         break;
       case 'result':
-        this.zerar = 0;
-        this.resultado = n;
+        this.v1 = 0;
+        this.v2 = n;
         this.numero = 'number';
     }
   }
+  // Função que chama que seleciona os operadores
+  operacao(o: string) {
+    this.calcular();
+    this.operador = o
+    this.v1 = this.v2;
+    this.numero = 'operator';
+  }
+  // Função para que ao apertar o botão calcula e mostra o resultado
+  calcular(){
+    this.v2 = eval('' + this.v1 + this.operador + '(' + this.v2 + ')');
+    this.v1 = 0;
+    this.numero = 'result';
+    this.operador = '+';
+    this.decimal = false;
+    this.decimals = 0;
+  }
+  // Função que apagas os dados digitados anteriormente na calculadora
+  apagar() {
+    this.v2 = 0;
+    this.v1 = 0;
+    this.numero = 'number';
+    this.decimal = false;
+    this.decimals = 0;
+  }
 	}
+
+  
 
  
 
   
+
+
 
